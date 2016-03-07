e703c591792ded5448a73e796f72fdb8b36f5bfc
rockblus <89369750@qq.com>
2016-03-07 15:38:33 +0800

    统一数据库配置

--- FrontApplication/Common/Conf/config.php
+++ FrontApplication/Common/Conf/config.php
@@ -9,9 +9,9 @@ return array(
     'db_type' => 'mongo',
     'db_host' => '127.0.0.1',
     'db_port' => '27017',
-    'db_Name' => 'oeoe',
-    'DB_USER'   => 'rockblus', // 用户名
-    'DB_PWD'    => 'HDZrockblus8', // 密码
+    'db_Name' => 'dipan',
+//    'DB_USER'   => 'rockblus', // 用户名
+//    'DB_PWD'    => 'HDZrockblus8', // 密码
     'MODULE_ALLOW_LIST' => array (
         'Home','Member','Mongod','Api'
     ),
