a4296b47862172ae8754c19063325a3fcc00225e
rockblus <89369750@qq.com>
2016-03-25 07:25:55 +0800

    hack title space

--- FrontApplication/Home/Controller/IndexController.class.php
+++ FrontApplication/Home/Controller/IndexController.class.php
@@ -63,7 +63,7 @@ class IndexController extends CommonController
         }
         //如果没有关键词
         if (empty($re['two'])) {
-            $seoTitle = $_SESSION['place']['thisCityInfo']['name'] . '地盘 diPan.So';
+            $seoTitle = $_SESSION['place']['thisCityInfo']['name'] . '地盘_diPan.So';
             $pinDao = $_SESSION['place']['thisCityInfo']['name'];//分类聊天频道字符串
 
             $this->indexAllRe['pinDao'] = $pinDao;
