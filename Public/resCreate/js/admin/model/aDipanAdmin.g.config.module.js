8d91ba594c582a8eb7b71f042b7b67d91ca507bb
rockblus <89369750@qq.com>
2016-05-18 17:24:39 +0800

    添加10个线程

--- Public/resCreate/js/admin/model/aDipanAdmin.g.config.module.js
+++ Public/resCreate/js/admin/model/aDipanAdmin.g.config.module.js
@@ -31,11 +31,60 @@
      * 16/3/8 */
 
     function config() {
+        var port = _getPort();
         return {
             host: {//host 配置
-                nodeHost: 'http://www.dipan.so:3008'//nodejsApi hostUrl
+                nodeHost: 'http://www.dipan.so:' + port//nodejsApi hostUrl
             }
         };
     }
+
+    /**************************
+     * 获取当前url端口号
+     * 16-5-17 上午8:43 ByRockBlus
+     **************************/
+    function _getPort() {
+        var url = window.location.href;
+        var port;
+        url = url.split(':');
+        url = url[2];
+        url = url.split('/');
+        url = url[0];
+
+        switch (url) {
+            case '8082':
+                port = '3082';
+                break;
+            case '8083':
+                port = '3083';
+                break;
+            case '8084':
+                port = '3084';
+                break;
+            case '8085':
+                port = '3085';
+                break;
+            case '8086':
+                port = '3086';
+                break;
+            case '8087':
+                port = '3087';
+                break;
+            case '8088':
+                port = '3088';
+                break;
+            case '8089':
+                port = '3089';
+                break;
+            case '8090':
+                port = '3090';
+                break;
+            case '8091':
+                port = '3091';
+                break;
+        }
+        return port;
+    }
+
 
 })(window, document);
