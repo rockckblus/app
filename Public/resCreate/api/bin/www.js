02d71bf05c98f6794d2f939a99c48b4b9e07b264
马工 <89369750@qq.com>
2016-06-30 06:59:51 +0800

    40

--- Public/resCreate/api/bin/www.js
+++ Public/resCreate/api/bin/www.js
@@ -26,12 +26,13 @@ var server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
-if (cluster.isMaster222) {//多cpu线程
+if (cluster.isMaster) {//多cpu线程
+    server.listen(port);
 //    console.log('masgter start');
     //Fork workers;
-    for (var i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
+    //for (var i = 0; i < numCPUs; i++) {
+    //    cluster.fork();
+    //}
 
     cluster.on('listening', function (worker, address) {
         console.log('listening:worker' + worker.process.pid + ', Addresss: ' + address.address + ':' + address.port);
