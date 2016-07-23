6c8cec59c840a98fce960dfd009e2ad86605bcba
马工 <89369750@qq.com>
2016-07-23 17:29:22 +0800

    sub

---
+++ Public/app/dist/js/appDev.js
@@ -0,0 +1,45 @@
+/** 开发js  */
+
+/** 库  */
+document.write('<script src="../../dist/js/res/angular.min.js"><\/script>');
+document.write('<script src="../../dist/js/res/angular-ui-router.min.js"><\/script>');
+document.write('<script src="../../dist/js/res/bindonce.min.js"><\/script>');
+//
+///** module  */
+//document.write('<script src="/Public/resCreate/js/app/src/aModule/aDipan.g.config.module.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/aModule/uiBlock.dipan.uiGroup.module.js"><\/script>');
+//
+//document.write('<script src="/Public/resCreate/js/app/src/aModule/uiBlock/alertDiv.uiBlock.alertUi.module.js"><\/script>');
+//
+//
+///** controller  */
+//document.write('<script src="/Public/resCreate/js/app/src/controller/dipan.g.body.controller.js"><\/script>');
+//
+///** directive  */
+//document.write('<script src="/Public/resCreate/js/app/src/directive/angularEnd.dipan.angularEnd.directive.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/directive/clickRouterUiBlock.uiBlock.router.directive.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/directive/urlParse.dipan.firstNginx.directive.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/directive/areaKey/areaKey.dipan.topAreaKey.directive.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/directive/areaKey/areaShow.dipan.areaShowSelect.directive.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/directive/areaKey/keyShow.dipan.keyShowSelect.directive.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/directive/block1/leftNav.dipan.Block1LeftNav.directive.js"><\/script>');
+//
+//document.write('<script src="/Public/resCreate/js/app/src/directive/uiBlock/alertDiv/alertInfo.alertDiv.showInfo.directive.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/directive/uiBlock/alertDiv/alertWarn.alertDiv.alertWarn.directive.js"><\/script>');
+//
+///** filter  */
+//document.write('<script src="/Public/resCreate/js/app/src/filter/toHtml.dipan.toHtml.filter.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/filter/editCategoryServiceNav.dipan.editCategoryServiceNav.filter.js"><\/script>');
+//
+///** server  */
+//document.write('<script src="/Public/resCreate/js/app/src/server/dipan.fun.reBindOnce.factory.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/server/alertDiv.uiBlock.alertUi.factory.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/server/compile.uiBlock.compile.factory.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/server/tools.dipan.block.factory.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/server/urlParse.dipan.firstData.factory.js"><\/script>');
+//document.write('<script src="/Public/resCreate/js/app/src/server/apiPost.dipan.postUrl.factory.js"><\/script>');
+//
+///** tpl  */
+//document.write('<script src="/Public/resCreate/js/app/dist/app.templates.js"><\/script>');
+//
+
---
+++ Public/app/src/js/controller/dipan.g.body.controller.js
@@ -0,0 +1,38 @@
+(function () {
+    'use strict';
+
+    /**
+     * body 控制器
+     * 16/2/1 */
+    angular.module('dipan').controller('body', body);
+
+    angular.module('dipan').controller('header', header);
+    header.$inject = ['$scope', 'urlParse', 'api'];
+    function header($scope, urlParse, api) {
+        $scope.title = '';
+        $scope.$on('urlParseChangeSub', function () {
+            try {
+                $scope.title = urlParse.data.seoTitle;
+            } catch (e) {
+                console.error(e);
+            }
+        });
+
+
+    }
+
+
+    /**
+     * 手动注入
+     * 16/2/1 */
+    body.$inject = ['$scope', '$timeout'];
+
+    /**
+     * controllerFun
+     * 16/2/1 */
+    function body($scope, $timeout, repBindOnce) {
+
+    }
+
+
+})();
