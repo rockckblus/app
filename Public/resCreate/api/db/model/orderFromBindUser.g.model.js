5194fe4fd7fb3fe8e3420ca6967420a839533398
rockblus <89369750@qq.com>
2016-12-15 06:49:14 +0800

    sub

---
+++ Public/resCreate/api/db/model/orderFromBindUser.g.model.js
@@ -0,0 +1,50 @@
+/**
+ * 订单的用户 关联 表 ,
+ *  */
+
+
+var g = require('../../g.config');
+
+/**
+ * 订单的用户关联表
+ * 16/3/7 */
+var orderFromBindUserSchema = new g.Schema({
+    orderId: {type: g.Schema.Types.ObjectId},//订单id
+    orderUid: {type: g.Schema.Types.ObjectId},//发订单的用户id
+    bindUid: {type: g.Schema.Types.ObjectId},//抢订单的用户id
+    bindUidType: {type: Number, default: 1},//抢订单的用户，对应订单 的 关系 1 主动接单(点击接单) ，2 。被动接单(点击下单) 3.被选单
+    selectOrderTime:{type:Date},//被选单时间
+    clickJieOrderTime:{type:Date}, //点击接单时间
+    clickDownOrderTime:{type:Date},//点击下单时间
+});
+
+
+/**
+ * orderFromBindUserSchema
+ * 16/3/7 */
+var orderFromBindUserModel= g.mongoose.model('orderFrom.bindUser', orderFromBindUserSchema , 'orderFrom.bindUser');
+
+module.exports = orderFromBindUserModel;
+
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * 16/3/7 */
+
+
